26e2e87c665e22bb8e50fb383279e741d1df7573
Developer <68657182+kartvya@users.noreply.github.com>
2025-08-12 12:57:57 +0530

    some changes

--- src/redux/actions/index.js
+++
@@ -1,7 +0,0 @@
-export * from './auth';
-export * from './journal';
-export * from './nutrition';
-export * from './recreation';
-export * from './calendar';
-export * from './exercise';
-export * from './todo';
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -38,7 +38,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
       immutableCheck: false,
