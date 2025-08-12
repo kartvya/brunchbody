f24e8c5f7624d0ac0aa221ea4a355eac97383bfd
Developer <68657182+kartvya@users.noreply.github.com>
2025-08-12 13:47:18 +0530

    fonts integrated

--- src/screens/completeProfile/CompleteProfileWrapper.js
+++ src/screens/completeProfile/CompleteProfileWrapper.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const CompleteProfileWrapper = () => {
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Icon name="rocket" size={30} color="#900" />
       <Text>CompleteProfileWrapper</Text>
     </View>
   );
