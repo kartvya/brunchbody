3ced183bbc570ca2423fb8d47f7efaac14113cb3
Developer <68657182+kartvya@users.noreply.github.com>
2025-08-12 15:13:26 +0530

    Component up and running

--- src/root-container/RootContainer.js
+++ src/root-container/RootContainer.js
@@ -3,13 +3,19 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import RootNavigation from '../navigation/RootNavigation';
 import { persistor, store } from '../redux';
+import { PaperProvider } from 'react-native-paper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export class RootContainer extends Component {
   render() {
     return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <RootNavigation />
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <PaperProvider>
+              <RootNavigation />
+            </PaperProvider>
+          </GestureHandlerRootView>
         </PersistGate>
       </Provider>
     );
